a7c66bd7118ed9899db2a1471db25adfee6169af
nasim <nasim@10.0.0.10>
2023-04-08 02:47:09 -0400

    Added add()

--- src/day2Project5/Day2HWProject5.java
+++ src/day2Project5/Day2HWProject5.java
@@ -6,6 +6,14 @@ public class Day2HWProject5 {
 		
 		
 		System.out.println("day2 HW project5");
+		
+		Day2HWProject5 obj = new Day2HWProject5();
+		System.out.println(obj.add(2, 4));
+	}
+	
+	
+	protected int add (int a, int b) {
+		return a+b;
 	}
 
 }
